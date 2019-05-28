b71605e167f6df36160b5d88df63cfa760b7e48c
ymhgithub <2573899087@qq.com>
2019-05-28 15:37:24 +0800

    5月28日提交

--- src/main/java/org/game/beatingpicture/controller/RegistController.java
+++ src/main/java/org/game/beatingpicture/controller/RegistController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
